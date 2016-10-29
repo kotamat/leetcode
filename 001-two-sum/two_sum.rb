e419aa4d8a6407b1f0896c355ea5c4aa62dc1f58
kota-matsumoto <kota-matsumoto@altplus.co.jp>
2016-10-29 09:53:47 +0900

    add benchmark and speed up

--- 001-two-sum/two_sum.rb
+++ 001-two-sum/two_sum.rb
@@ -1,3 +1,4 @@
+require 'benchmark'
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
@@ -5,14 +6,24 @@ def two_sum(nums, target)
   pool = {}
   i = nums.length - 1
   while n = nums.pop
-    break if pool[n]
+    return [nums.length, pool[n]] if pool[n]
     pool[target - n] = i
     i -= 1
   end
-  [nums.length, pool[n]]
 end
+
+iterate_num = 1000000
 
 nums = [0, 3, 2, 5, 0, 10]
 target = 7
 
+Benchmark.bm do |r|
+  r.report "two sum" do
+    iterate_num.times do
+      two_sum(nums, target)
+    end
+  end
+end
+nums = [0, 3, 2, 5, 0, 10]
+target = 7
 p two_sum(nums, target)
