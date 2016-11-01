292121bcfebc0682ae74d17f4aaf25c3edd2ce94
kotamat <kota1861@gmail.com>
2016-11-02 02:24:41 +0900

    speedup 002

--- 002-add-two-numbers/add-two-numbers.go
+++ 002-add-two-numbers/add-two-numbers.go
@@ -1,49 +1,36 @@
 package _02_add_two_numbers
 
+
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	result := &ListNode{}
-	carry := 0
-	cl1, cl2, crl := l1, l2, result
-	for {
-		l1val := 0
-		l2val := 0
-		if cl1 != nil {
-			l1val = cl1.Val
-		}
-		if cl2 != nil {
-			l2val = cl2.Val
-		}
-
-		sum := l1val + l2val + carry
-		crl.Val = sum % 10
-
+	tracer := l1
+	carry, sum := 0, 0
+	for  {
+		sum = carry + tracer.Val + l2.Val
 		carry = sum / 10
-
-		if cl1 != nil {
-			cl1 = cl1.Next
-		} else {
-			cl1 = nil
-		}
-		if cl2 != nil {
-			cl2 = cl2.Next
-		} else {
-			cl2 = nil
-		}
-
-		if cl1 == nil && cl2 == nil {
+		tracer.Val = sum - carry * 10
+		if tracer.Next == nil || l2.Next == nil{
+			if tracer.Next == nil {
+				tracer.Next = l2.Next
+			}
+			for carry > 0 {
+				if tracer.Next == nil {
+					tracer.Next = &ListNode{Val: carry, Next: nil}
+					break
+				}
+				tracer = tracer.Next
+				sum = carry + tracer.Val
+				carry = sum / 10
+				tracer.Val = sum - carry * 10
+			}
 			break
 		}
-		crl.Next = &ListNode{}
-		crl = crl.Next
+		tracer, l2 = tracer.Next, l2.Next
 	}
 
-	if carry > 0 {
-		crl.Next = &ListNode{Val: carry, Next: nil}
-	}
-	return result
+	return l1
 }
--- 002-add-two-numbers/add_two_numbers_test.go
+++ 002-add-two-numbers/add_two_numbers_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestAddTwoNumbers(t *testing.T)  {
-	l1 := &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: nil}}}
+	l1 := &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: nil}}}
 	l2 := &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4, Next: nil}}}
-	answer := &ListNode{Val: 7, Next:&ListNode{Val:0, Next: &ListNode{Val:8, Next:nil}}}
+	answer := &ListNode{Val: 7, Next:&ListNode{Val:0, Next: &ListNode{Val:0, Next: &ListNode{Val: 1, Next:nil}}}}
 	li := addTwoNumbers(l1, l2)
 	assert.Equal(t, li, answer)
 }
