51c86ef97705c9d240299bd1c0836fdd56a83b29
kota-matsumoto <kota-matsumoto@altplus.co.jp>
2016-12-02 01:51:58 +0900

    005

--- 005-longest-palindromic-substring/code.go
+++ 005-longest-palindromic-substring/code.go
@@ -1,52 +1,45 @@
 package _05_longest_palindromic_substring
 
 func longestPalindrome(s string) string {
-	longestPalindrome := []rune{}
-	sr := []rune(s)
+	sLen := len(s)
+	if sLen == 1 {
+		return s
+	}
+	LLi := 0
+	LRi := 0
 
-	for key := range sr {
-		// 真ん中がvalueの場合
-		palindrome := []rune{}
-		Li := key
-		Ri := key
-
-		for {
-			if sr[Li] == sr[Ri] {
-				palindrome = sr[Li : Ri + 1]
-			}
-			if Li <= 0 || Ri >= len(sr) - 1 || sr[Li] != sr[Ri] {
+	calc := func(Li int, Ri int) {
+		PLi := 0
+		PRi := 0
+		for ; Li >= 0 && Ri < sLen; {
+			if s[Li] != s[Ri] {
 				break
 			}
 			Li--
 			Ri++
 		}
+		PLi = Li + 1
+		PRi = Ri - 1
 
-		if len(palindrome) > len(longestPalindrome) {
-			longestPalindrome = palindrome
+		if PRi - PLi > LRi - LLi {
+			LRi = PRi
+			LLi = PLi
 		}
+	}
+
+	for key := range s {
+		if sLen - (LRi - LLi) / 2 < key {
+			break
+		}
+		// 真ん中がvalueの場合
+		calc(key, key)
 
 		// 真ん中がvalueとvalue -1の場合
 		if key < 1 {
 			continue
 		}
-		palindrome = []rune{}
-		Li = key - 1
-		Ri = key
-		for {
-			if sr[Li] == sr[Ri] {
-				palindrome = sr[Li : Ri + 1]
-			}
-			if Li <= 0 || Ri >= len(sr) - 1 || sr[Li] != sr[Ri] {
-				break
-			}
-			Li--
-			Ri++
-		}
-
-		if len(palindrome) > len(longestPalindrome) {
-			longestPalindrome = palindrome
-		}
+		calc(key - 1, key)
 	}
 
-	return string(longestPalindrome)
+	return string(s[LLi:LRi + 1])
 }
--- 005-longest-palindromic-substring/code_test.go
+++ 005-longest-palindromic-substring/code_test.go
@@ -10,3 +10,19 @@ func TestCode(t *testing.T) {
 	answer := "bab"
 	assert.Equal(t, answer, longestPalindrome(s))
 }
+func TestCode2(t *testing.T) {
+	s := "cbbd"
+	answer := "bb"
+	assert.Equal(t, answer, longestPalindrome(s))
+}
+func TestCode3(t *testing.T) {
+	s := "abababababa"
+	answer := "abababababa"
+	assert.Equal(t, answer, longestPalindrome(s))
+}
+func BenchmarkCode(t *testing.B) {
+	for i := 0; i < t.N; i++ {
+		s := "babad"
+		longestPalindrome(s)
+	}
+}
